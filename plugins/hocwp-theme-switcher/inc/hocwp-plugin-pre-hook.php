41890a610627339fde4e400b689c7d6fb84caaf9
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2015-10-08 18:31:41 -0700

    Commit HocWP Theme Switcher version 1.1

--- plugins/hocwp-theme-switcher/inc/hocwp-plugin-pre-hook.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dicu
- * Date: 07/10/2015
- * Time: 5:16 AM
- */
--- plugins/hocwp-theme-switcher/load.php
+++ plugins/hocwp-theme-switcher/load.php
@@ -1,11 +1,24 @@
 <?php
 require_once(HOCWP_THEME_SWITCHER_INC_PATH . '/hocwp-plugin-pre-hook.php');
 
+function hocwp_theme_switcher_missing_core_notice() {
+    ?>
+    <div class="updated notice settings-error error">
+        <p><strong><?php _e('Error:', 'hocwp'); ?></strong> <?php _e('Plugin HocWP Theme Switcher cannot be run properly because of missing core.', 'hocwp'); ?></p>
+    </div>
+    <?php
+}
+
 if(!defined('HOCWP_PATH')) {
     $path = get_template_directory() . '/hocwp/load.php';
 
     if(!file_exists($path)) {
         $path = HOCWP_THEME_SWITCHER_PATH . '/hocwp/load.php';
+    }
+
+    if(!file_exists($path)) {
+        add_action('admin_notices', 'hocwp_theme_switcher_missing_core_notice');
+        return;
     }
 
     require_once($path);
