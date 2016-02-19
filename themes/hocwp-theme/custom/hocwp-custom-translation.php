216637f0bcc0d976ea6402e9c5d3b11bc81677af
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2016-02-19 21:26:08 +0700

    Re-update theme custom translation file

--- themes/hocwp-theme/custom/hocwp-custom-translation.php
+++ themes/hocwp-theme/custom/hocwp-custom-translation.php
@@ -1,11 +1,2 @@
 <?php
 if(!function_exists('add_filter')) exit;
-$theme_options = get_option('hocwp_theme_setting');
-$footer_text = hocwp_get_value_by_key($theme_options, 'footer_text');
-$sub_promo = hocwp_get_value_by_key($theme_options, 'sub_promo');
-hocwp_theme_register_translation_text('hocwp_footer_text', $footer_text, true);
-hocwp_theme_register_translation_text('hocwp_sub_promo', $sub_promo, true);
-hocwp_theme_register_translation_text('hocwp_my_account', 'My Account');
-hocwp_theme_register_translation_text('hocwp_logout', 'Logout');
-hocwp_theme_register_translation_text('hocwp_my_account_login', 'My Account / Login');
-hocwp_theme_register_translation_text('hocwp_register', 'Register');
